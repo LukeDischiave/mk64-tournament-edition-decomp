bf9c4e1e3f9e2a42f2147ceaf0014a41ca65f3e9
LukeDischiave <lukedischiave14@gmail.com>
2026-09-03 09:33:18 -0400

    updated build date and number

--- src/menu_items.c
+++ src/menu_items.c
@@ -5972,7 +5972,7 @@ void render_custom_overlay(void) {
     print_text1_center_mode_1(x, y - 0x28, "WEATHERTON  ABNEY  CLIMATEE  ZSERF  DNTN31", 0, 0.60f, 0.60f);
 
     // version / date (smaller) - left-aligned to start under 'KART'
-    print_text1_left(x + 0x78, y + 0x06, "TE V2026-08-28 1.6b", 0, 0.65f, 0.65f);
+    print_text1_left(x + 0x78, y + 0x06, "TE V2026-09-03 1.1 dev", 0, 0.65f, 0.65f);
 
     // option name placeholders (second column) and values (third column)
     for (i = 0; i < CUSTOM_MENU_ROWS; i++) {
